495af51900e3e9c507d82c38c422ea47ff6767e8
kakyoin <jo999dio@sina.com>
2015-09-29 18:00:50 +0800

    第四章模板应用

--- dio/urls.py
+++ dio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-]
+from django.conf.urls import *
+from dio.views import hello, current_datetime, hours_ahead
+urlpatterns = patterns('',
+              ('^hello/$', hello),
+              ('^time/$', current_datetime),
+              (r'^time/plus/(\d{1,2})/$', hours_ahead),
+)
---
+++ dio/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse,Http404
+import datetime
+from django.shortcuts import render_to_response
+
+
+def hello(request):
+    return HttpResponse("Hello world")
+
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    return render_to_response('current_datetime.html', {'current_date': now})
+
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
